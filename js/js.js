537ec038c1692807ea1d882bb162c80664932561
Paul H Howells <p.h.howells at gmail dot com>
2013-01-21 15:05:36 +0000

    printing out em font-size combinations, that work at one level deep

--- js/js.js
+++ js/js.js
@@ -48,8 +48,8 @@ Copyright Paul H Howells
         j_permutation,
         j_class;
 
-      array = [12, 16];
-
+      // array = [12, 16, 20];
+      array = [12, 18];
 
       // test how the generated CSS works by creating a set of class use permutations to experiment with
       // loop through all the nesting permutations to the nesting_depth depth desired and build sets of nested divs for testing
@@ -65,42 +65,79 @@ Copyright Paul H Howells
         for (j = 0; j < nesting_depth; j += 1) {
           j_permutation = base_array[base_array.length - (j + 1)] || 0;
           j_class = 'f-' + array[j_permutation];
-          $permutation.wrapInner($('<div/>').html(j_class).addClass(j_class));
+          $permutation.wrapInner($('<span/>').html(j_class).addClass(j_class));
         }
         $('#test').append($permutation);
       }
       // end of test permutations
 
 
-      // roughing out with pixels to test code
-      // will replace with loops and ems
       var
         rule = "",
         css = "",
         css_html = "",
-        style;
+        style,
+        i_font_size_px,
+        i_font_size_em,
+        line_height_px,
+        i_line_height_em,
+        tab = '&#9;';
       
-      rule = ".f-12 {line-height: 20px}";
-      css += rule;
-      css_html += rule + "<br />";
-
-      rule = ".f-16 {line-height: 20px}";
-      css += rule;
-      css_html += rule + "<br />";
+      var line_height_px = 30;
+      
+      /* base font-sizes and line-heights */      
+      css_html += "<br />";
+      css_html += "/* base font sizes */<br />";
+      
+      for (i = 0; i < array.length; i += 1) {
+        i_font_size_px = array[i];
+        i_font_size_em = i_font_size_px / 16;
+        i_line_height_em = line_height_px / array[i];
+ 
+        rule = ".f-" + i_font_size_px + " {"
+        css += rule;
+        css_html += rule + "<br />";
+        
+        rule = "font-size: " + i_font_size_em + "em;";
+        css += rule;
+        css_html += tab + rule + "<br />";
+        
+        rule = "line-height: " + i_line_height_em + "em;"
+        css += rule;
+        css_html += tab + rule + "<br />";
+        
+        rule = "}";
+        css += rule;
+        css_html += rule + "<br />";
+      }
+      
+      css_html += "<br />";
+      css_html += "/* font size combinations */<br />";
+      
+      for (i = 0; i < array.length; i += 1) {
+        i_font_size_px = array[i];
+        
+        for (j = 0; j < array.length; j += 1) {
+          j_font_size_px = array[j];
+          j_font_size_em = j_font_size_px / i_font_size_px;
+          
+          rule = ".f-" + i_font_size_px + " .f-" + j_font_size_px + " {font-size: " + j_font_size_em + "em;}";
+          css += rule;
+          css_html += rule + "<br />";  
+        }
+      }
       
       
-      rule = ".f-12 {font-size: 12px}";
-      css += rule;
-      css_html += rule + "<br />";
-
+      /*
       rule = ".f-16 {font-size: 16px}";
       css += rule;
       css_html += rule + "<br />";
-
+      */
+      
       $('#output').html(css_html);
       
       // this bit probably won't work in IE7
-      // however I doubt there are any web developers using IE7 as their primary browser
+      // however I hope there are not many web developers using IE7 as their primary browser in 2012
       style = document.createElement('style');
       style.type = 'text/css';
       style.innerHTML = css;
